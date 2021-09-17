1cda7037c893cfd9963f4d8b2e693387b8715822
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-09-18 00:37:41 +0530

    index.js file added

---
+++ index.js
@@ -0,0 +1,31 @@
+// make a scoreboard 
+let user = 0
+let computer = 0
+const userScore = document.getElementById("userScore")
+const computerScore = document.getElementById("compScore")
+const rock = document.getElementById("r")
+const paper = document.getElementById("p")
+const scissor = document.getElementById("s")
+
+console.log("Hello")
+rock.addEventListener("click", ()=>{
+
+    console.log("rock");
+
+})
+paper.addEventListener("click", ()=>{
+
+    console.log("paper");
+
+})
+scissor.addEventListener("click", ()=>{
+
+    console.log("scissor");
+
+})
+
+
+function main() {
+
+
+}
