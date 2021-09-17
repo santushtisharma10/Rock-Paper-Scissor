3366889232e89cb12dcb7f232fb3c4d0a53803f0
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-09-18 02:08:16 +0530

    changes made

--- index.js
+++ index.js
@@ -6,21 +6,77 @@ const computerScore = document.getElementById("compScore")
 const rock = document.getElementById("r")
 const paper = document.getElementById("p")
 const scissor = document.getElementById("s")
+const userImg = document.getElementById("user-mainImg");
 
-console.log("Hello")
+const choice = () => {
+
+    const arr = ["r", "p", "s"]
+    return arr[Math.floor(Math.random()*3)];
+
+}
+
+const win = () => {
+
+    user += 1;
+    console.log(user, userScore.innerHTML)
+    console.log("win")
+    userScore.innerHTML = user;
+    //computerScore.innerHTML = computer;
+
+}
+const lose = () => {
+
+    computer++;
+    console.log(computer, computerScore.innerHTML)
+    console.log("lose")
+    computerScore.innerHTML = computer;
+    //userScore.innerHTML = user;
+
+}
+
+const playGame = (userChoice) => {
+    const compChoice = choice()
+    console.log(userChoice, compChoice);
+    switch(compChoice+userChoice) {
+
+        case "sp":
+        case "rs":
+        case "pr":
+                  lose()
+                  break;
+        case "sr":
+        case "rp":
+        case "ps":
+                  win()
+                  break;
+        case "ss":
+        case "rr":
+        case "pp":console.log("Draw")
+                  break;
+    }
+    
+    
+}
+
 rock.addEventListener("click", ()=>{
 
     console.log("rock");
+    userImg.src = "images/rock-left.png";
+    playGame("r")
 
 })
 paper.addEventListener("click", ()=>{
 
     console.log("paper");
+    userImg.src = "images/paper-lefte.png"
+    playGame("p")
 
 })
 scissor.addEventListener("click", ()=>{
 
     console.log("scissor");
+    userImg.src = "images/scissor-left.png"
+    playGame("s")
 
 })
 
